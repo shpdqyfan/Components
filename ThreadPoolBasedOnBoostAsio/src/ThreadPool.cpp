a04fc9dea798c33960243e8a50e9c213f2bd7d25
Yangfan <qyfan88@163.com>
2017-11-18 21:27:53 +0800

    Update ThreadPool.cpp

--- ThreadPoolBasedOnBoostAsio/src/ThreadPool.cpp
+++ ThreadPoolBasedOnBoostAsio/src/ThreadPool.cpp
@@ -3,6 +3,7 @@
     Author: shpdqyfan
     profile: Thread pool based on boost asio lib
 */
+#include <iostream>
 
 #include "ThreadPool.h"
 
@@ -10,21 +11,29 @@ ThreadPool::ThreadPool(size_t threadNum)
     : myIoService()
     , myIoServiceWorker(new AsioServiceWorker::element_type(myIoService))
 {
+    std::cout<<"ThreadPool, construct"<<std::endl;
+        
     for(size_t i = 0;i < threadNum;++i)
     {
+        //The run() of myIoService will be invoked by multiple threads, 
+        //so that the "task list" of myIoService can be shared.
         myThreadGroup.add_thread(new boost::thread(boost::bind(&boost::asio::io_service::run, &myIoService)));
     }
 }
 
 ThreadPool::~ThreadPool()
 {
+    std::cout<<"ThreadPool, deconstruct"<<std::endl;
+    
     myIoService.reset();
     myThreadGroup.join_all();
-    myIoService.stop();
+    //Stop the io_service object's event processing loop.
+    myIoService.stop(); 
 }
 
 void ThreadPool::post(ThreadCallBack cb)
 {
+    //The posted cb will only be called in a thread in which run() of io_service is currently being invoked.
     myIoService.post(cb);
 }
 
